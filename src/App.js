3d412f06edb87bfee17424ca3ada43afea3d0b59
Chilex23 <onumaegbuchilex@gmail.com>
2022-05-25 11:39:50 +0100

    Search Functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -10,6 +11,8 @@ class App extends Component {
       monsters: [],
       searchField: ''
     }
+
+    // this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -18,11 +21,21 @@ class App extends Component {
     .then(users => this.setState({monsters: users}))
   }
 
+  handleChange = e => { this.setState({ searchField: e.target.value }) }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
     return (
       <div className="App">
-        <input type="search" placeholder="Search for monsters" onChange={e => this.setState({ searchField: e.target.value })} />
-        <CardList monsters={this.state.monsters} /> 
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+          placeholder="Search for monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} /> 
       </div>
     );
   }
